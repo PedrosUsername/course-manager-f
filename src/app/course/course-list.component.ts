62c67a591ae92a7357b082dad000545e82da9ed4
rogs <pilique11@gmail.com>
2021-09-02 04:28:49 -0300

    courses list table ok

---
+++ src/app/course/course-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core'
+import { Course } from './course'
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+})
+export class CourseListComponent implements OnInit{
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+      this.courses = [
+          {
+              id: 1,
+              name: 'Angular: forms',
+              imageUrl: '',
+              price: 99.99,
+              code: 'XPS-8996',
+              duration: 120,
+              rating: 5.4,
+              releaseDate: '25-12-2000'
+          },
+          {
+              id: 2,
+              name: 'Angular: http',
+              imageUrl: '',
+              price: 45.99,
+              code: 'LKL-8996',
+              duration: 80,
+              rating: 4,
+              releaseDate: '20-09-2001'              
+          }
+      ]
+  }
+}
