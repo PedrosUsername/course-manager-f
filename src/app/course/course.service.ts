a33262ba9c1035b45aaf01a1665a3273e630de44
rogs <pilique11@gmail.com>
2021-09-07 05:17:00 -0300

    UPDATE course ok

--- src/app/course/course.service.ts
+++ src/app/course/course.service.ts
@@ -10,6 +10,16 @@ export class CourseService {
         return COURSES;
     }
 
+    retrieveById(id: number): Course {
+        return COURSES.find((courseIterator: Course) => courseIterator.id === id)!;
+    }
+
+    save(course: Course): void{
+        if(course.id){
+            const index = COURSES.findIndex((courseIterator: Course) => courseIterator.id === course.id);
+        }
+    }
+
 }
 
 var COURSES: Course[] = [
@@ -17,6 +27,7 @@ var COURSES: Course[] = [
         id: 1,
         name: 'Angular: CLI',
         releaseDate: 'November 2, 2019',
+        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
         duration: 120,
         code: 'XLF-1212',
         rating: 3,
@@ -27,6 +38,7 @@ var COURSES: Course[] = [
         id: 2,
         name: 'Angular: Forms',
         releaseDate: 'November 4, 2019',
+        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Forms.',
         duration: 80,
         code: 'DWQ-3412',
         rating: 3.5,
@@ -37,6 +49,7 @@ var COURSES: Course[] = [
         id: 3,
         name: 'Angular: HTTP',
         releaseDate: 'November 8, 2019',
+        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de HTTP.',
         duration: 80,
         code: 'QPL-0913',
         rating: 4.0,
@@ -47,6 +60,7 @@ var COURSES: Course[] = [
         id: 4,
         name: 'Angular: Router',
         releaseDate: 'November 16, 2019',
+        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Router.',
         duration: 80,
         code: 'OHP-1095',
         rating: 4.5,
@@ -57,6 +71,7 @@ var COURSES: Course[] = [
         id: 5,
         name: 'Angular: Animations',
         releaseDate: 'November 25, 2019',
+        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis sobre Animation.',
         duration: 80,
         code: 'PWY-9381',
         rating: 5,
