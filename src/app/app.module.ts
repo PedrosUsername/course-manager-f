cc9d45c31d8b4dd6eb088031ef572721df6b9980
rogs <pilique11@gmail.com>
2021-09-09 06:23:52 -0300

    course module ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,25 @@ import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './course/course-list.component';
 import { StarComponent } from './star/star.component';
-import { ReplacePipe } from 'src/app/pipe/replace.pipe';
 import { Nav } from './nav/nav.component';
 import { RouterModule } from '@angular/router';
 import { E404Component } from './error/404/e404.component';
-import { CourseInfoComponent } from './course/course-info.component';
+import { CourseModule } from './course/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     Nav,
     E404Component,
-    CourseInfoComponent
   ],
   imports: [
+    CourseModule,
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
       },
       {
         path: '**', component: E404Component
